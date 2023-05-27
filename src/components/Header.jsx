e0051c4dd8804329529a07aea8e8142892a55c5a
colincheungkm <colincheung1993hk@gmail.com>
2023-05-27 15:14:57 -0400

    First commit and deploy

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between px-4 pt-4">
+      <h2>Enterprise FE Dashboard</h2>
+      <h2>Welcome Back, Colin</h2>
+    </div>
+  );
+};
+
+export default Header;
